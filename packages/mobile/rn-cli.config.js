d89f8960dc90a9574638e8399132ec1ab5dd4974
Paul Reichelt <paul.reichelt@outlook.com>
2018-09-06 14:52:44 +0200

    add mobile solution

---
+++ packages/mobile/rn-cli.config.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var path = require('path')
+const cwd = path.resolve(__dirname)
+
+var config = {
+  getProjectRoots() {
+    return getRoots()
+  },
+
+  /**
+   * Specify where to look for assets that are referenced using
+   * `image!<image_name>`. Asset directories for images referenced using
+   * `./<image.extension>` don't require any entry in here.
+   */
+  getAssetRoots() {
+    return getRoots()
+  },
+  extraNodeModules: {
+    crypto: path.resolve(cwd, './node_modules/react-native-crypto'),
+    stream: path.resolve(cwd, './node_modules/react-native-stream'),
+    randombytes: path.resolve(cwd, './node_modules/react-native-randombytes'),
+    vm: path.resolve(cwd, './node_modules/vm-browserify'),
+  },
+}
+
+function getRoots() {
+  return [
+    cwd, // current directory
+    path.resolve(cwd, '../..'), // project directory
+  ]
+}
+
+module.exports = config
---
+++ packages/mobile/src/App.js
@@ -0,0 +1,11 @@
+import { Demo } from '@vlek/plugins'
+import React from 'react'
+import { View } from 'react-native'
+
+export default () => {
+  return (
+    <View>
+      <Demo />
+    </View>
+  )
+}
